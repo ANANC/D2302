45fc41f31c2e18b2171a455133618e1dd146c903
KKKK <496007818@qq.com>
2023-11-30 23:25:03 +0800

    update:支持获取背包数据

---
+++ Source/D2302/Backpack/PropInstanceSubsystem.cpp
@@ -0,0 +1,31 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PropInstanceSubsystem.h"
+
+#include "D2302/Common/GameBaseInstance.h"
+
+void UPropInstanceSubsystem::Initialize(FSubsystemCollectionBase& Collection)
+{
+	
+}
+
+void UPropInstanceSubsystem::Deinitialize()
+{
+	
+}
+
+bool UPropInstanceSubsystem::GetPropConfigableInfo(FName propName,FPropTableConfigData& config)
+{
+	UGameBaseInstance* instance = Cast<UGameBaseInstance>(GetGameInstance());
+	if(instance && instance->PropTable)
+	{
+		FPropTableConfigData* configPtr = instance->PropTable->FindRow<FPropTableConfigData>(propName,"FPropTableConfigData");
+		if(configPtr)
+		{
+			config = *configPtr;
+			return true;
+		}
+	}
+	return false;
+}
---
+++ Source/D2302/Backpack/PropInstanceSubsystem.h
@@ -0,0 +1,28 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "BackpackTypes.h"
+#include "PropInstanceSubsystem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class D2302_API UPropInstanceSubsystem : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+
+
+public:
+	virtual void Initialize(FSubsystemCollectionBase& Collection) override;
+
+	virtual void Deinitialize() override;
+
+public:
+	
+	UFUNCTION(BlueprintPure)
+	bool GetPropConfigableInfo(FName propName,FPropTableConfigData& config);
+};
